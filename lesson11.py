d40671514ab948af1e138fcaadbd21abf14bf594
yair-stern <yairshte@gmail.com>
2025-01-01 16:04:45 +0200

    shorter program

--- lesson11.py
+++ lesson11.py
@@ -5,18 +5,13 @@ if __name__ == "__main__":
     arr2=[0,1,2,4,6,7,8,9,11,12,23,33,45,66,78,99]
     arr_solution = []
     ind1 = ind2 = 0
-    while True:
+    while ind1<len(arr1) and ind2<len(arr2):
         if arr1[ind1] == arr2[ind2]:
             arr_solution.append(arr1[ind1])
             ind1 +=1
             ind2 +=1
-            if ind1==len(arr1) or ind2==len(arr2): is_end=True
         elif arr1[ind1] < arr2[ind2]:
             ind1 +=1
-            if ind1==len(arr1): is_end=True
         elif arr1[ind1] > arr2[ind2]:
             ind2 +=1
-            if ind2==len(arr2): is_end=True
-        if is_end:
-            print (arr_solution)
-            exit()
+    print (arr_solution)
